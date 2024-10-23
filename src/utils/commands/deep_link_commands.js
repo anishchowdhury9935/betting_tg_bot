df0f488a43ed8514e06b4d89f8a3d3c61732458a
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-10-23 19:50:21 +0530

    save

--- src/utils/commands/deep_link_commands.js
+++ src/utils/commands/deep_link_commands.js
@@ -1,5 +1,6 @@
 const config = require("../../../config");
 const userBettingData = require("../../db/models/userBettingData");
+const UserRpsGameData = require("../../db/models/UserRpsGameData");
 const userDetails = require("../../db/models/userDetails");
 const { TryCatch, getChatId, getWalletBasicInfoToProceed, getTokenBalanceAsBettingAmount } = require("../../helper/helperMain");
 const { getUserMemeCoinBalanceObj } = require("../../helper/helperWeb3");
@@ -18,25 +19,33 @@ const index = {
             }
             const findBetDetails = await userBettingData.findById({ _id: bettingId });
             if (!findBetDetails) {
-                bot.sendMessage(chatId, 'This room does not exist ❌',{reply_to_message_id: replyToMessageId});
+                bot.sendMessage(chatId, 'This room does not exist ❌', { reply_to_message_id: replyToMessageId });
                 return;
             }
             if (findBetDetails.playersId.length === 2) {
-                bot.sendMessage(chatId, 'This room is full 🧑‍🤝‍🧑',{reply_to_message_id: replyToMessageId});
+                bot.sendMessage(chatId, 'This room is full 🧑‍🤝‍🧑', { reply_to_message_id: replyToMessageId });
                 return;
             }
             const findUser = await userDetails.findOne({ userName });
             if (findBetDetails.playersId.includes(findUser._id)) {
-                bot.sendMessage(chatId, 'You have already joined this room',{reply_to_message_id: replyToMessageId});
+                bot.sendMessage(chatId, 'You have already joined this room', { reply_to_message_id: replyToMessageId });
                 return;
             }
             const { balance } = await getTokenBalanceAsBettingAmount(userName);
             if (balance < findBetDetails.bettingAmount) {
-                bot.sendMessage(chatId, `Your balance is only :${balance}\n\n And your betting amount is :${findBetDetails.bettingAmount} \n\n please add some ${config.memeCoinInfo.name} by using:\n /wallet `,{reply_to_message_id: replyToMessageId});
+                bot.sendMessage(chatId, `Your balance is only :${balance}\n\n And your betting amount is :${findBetDetails.bettingAmount} \n\n please add some ${config.memeCoinInfo.name} by using:\n /wallet `, { reply_to_message_id: replyToMessageId });
                 return;
             }
             const addUserToBetRoom = await userBettingData.updateOne({ _id: bettingId }, { playersId: [...findBetDetails.playersId, findUser._id] })
-            bot.sendMessage(chatId, `@${userName} have betted ${findBetDetails.bettingAmount} ${config.memeCoinInfo.name} on ${findBetDetails.nameOfBet}\n\nClick on the link below to play the game:\n\nhttps://t.me/${config.botInfo.botTgUserName}?start=bettingId-${bettingId}_type-${'play'}_game-${findUser.bettingInfo.nameOfBet}\n\n you can use /mybettings to show the betting status`,{reply_to_message_id: replyToMessageId});
+            // const addUserToBetData = await UserRpsGameData.
+            const findUserBettingData = await userBettingData.findOne({ _id: bettingId });
+            if (!findUserBettingData) {
+                const newBet = await UserRpsGameData.create({ bettingId, playerRoundWin: [findUser._id,] });
+            } else {
+                const newArr = [...findUserBettingData.playersId, findUser._id];
+                const updateBet = await UserRpsGameData.updateOne({ bettingId }, { playerRoundWin: [...newArr] });
+            }
+            bot.sendMessage(chatId, `@${userName} have betted ${findBetDetails.bettingAmount} ${config.memeCoinInfo.name} on ${findBetDetails.nameOfBet}\n\nClick on the link below to play the game:\n\nhttps://t.me/${config.botInfo.botTgUserName}?start=bettingId-${bettingId}_type-${'play'}_game-${findUser.bettingInfo.nameOfBet}\n\n you can use /mybettings to show the betting status`, { reply_to_message_id: replyToMessageId });
         })
     },
     play: (bot, msg, match, dataExtractedThroughLink) => {
@@ -51,11 +60,11 @@ const index = {
             }
             const findBetDetails = await userBettingData.findById({ _id: bettingId });
             if (!findBetDetails) {
-                bot.sendMessage(chatId, 'This room does not exist ❌',{reply_to_message_id: replyToMessageId});
+                bot.sendMessage(chatId, 'This room does not exist ❌', { reply_to_message_id: replyToMessageId });
                 return;
             }
             if (!findBetDetails.playersId.includes(basicInfo.userBasicData._id)) {
-                bot.sendMessage(chatId, 'You are not a player of this game',{reply_to_message_id: replyToMessageId});
+                bot.sendMessage(chatId, 'You are not a player of this game', { reply_to_message_id: replyToMessageId });
                 return;
             }
             const replyMarkup = {
@@ -69,7 +78,7 @@ const index = {
                 ],
             };
             // console.log(`http:/localhost:5173/rps/${findBetDetails._id}/${type}/${findBetDetails.nameOfBet}/${basicInfo.userBasicData._id}`)
-            bot.sendMessage(chatId, `Play ${findBetDetails.nameOfBet} by clicking on button below:`, { reply_markup: replyMarkup,reply_to_message_id: replyToMessageId });
+            bot.sendMessage(chatId, `Play ${findBetDetails.nameOfBet} by clicking on button below:`, { reply_markup: replyMarkup, reply_to_message_id: replyToMessageId });
         })
     }
 }
