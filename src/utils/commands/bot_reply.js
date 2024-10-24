e9d529c6611a0b0a7a5cb7bac782f92480bbc284
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-10-24 09:16:48 +0530

    Update bot_reply.js

--- src/utils/commands/bot_reply.js
+++ src/utils/commands/bot_reply.js
@@ -55,7 +55,7 @@ const bot_reply = [
         func: (bot, msg) => {
             return TryCatch(async () => {
                 const replyToMessageId = msg.message_id;
-                const msgTxt = msg.text?.toLowerCase();
+                const msgTxt = msg.text?.toString().toLowerCase();
                 const userName = msg.from.username;
                 const chatId = getChatId(msg)
                 const findUser = await userDetails.findOne({ userName }).select(['_id']);
@@ -64,8 +64,8 @@ const bot_reply = [
                 if (!basicInfo.isAllTrue) {
                     return;
                 }
+                console.log(msgTxt)
                 globalVariables.games.map(({ commandName }) => {
-                    console.log(msgTxt)
                     if (commandName === msgTxt) {
                         isCommandRight = true;
                     }
